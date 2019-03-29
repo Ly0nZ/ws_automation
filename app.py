18ebb1d7cbfe545e2648c381e02d470c6159db7f
Ly0nZ <charleymlyons@gmail.com>
2019-03-28 18:20:15 -0700

    Added a process for a bar chart to be created from the newly created values, along with a max reference range for the bar chart to retrieve the correct number data values.

--- app.py
+++ app.py
@@ -12,8 +12,17 @@ def update_workbook(filename):
         new_cell = sheet.cell(row, 4)
         new_cell.value = commission
     
+    values = Reference(sheet,
+                       min_row=3,
+                       max_row=sheet.max_row,
+                       min_col=4,
+                       max_col=4)
+    
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'F3')
     workbook.save(filename)
 
 path = Path("sales")
 for file in path.glob('*.xlsx'):
-    process_workbook(file)
+    update_workbook(file)
