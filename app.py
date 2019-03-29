3043c1059831c4afbec2f83b556b76ff1ac2a764
Ly0nZ <charleymlyons@gmail.com>
2019-03-28 18:08:33 -0700

    Completed update_workbook method.

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ from pathlib import Path
 
 def update_workbook(filename):
     workbook = oxl.load_workbook(filename)
-    sheet = workbook['sheet1']
+    sheet = workbook['Sheet1']
+
+    for row in range(3, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        commission = cell.value * 0.1
+
+        new_cell = sheet.cell(row, 4)
+        new_cell.value = commission
     
+    workbook.save(filename)
